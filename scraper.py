584d3ef2724378b69667cf18c385b06f76ae8ec9
Petr Bouchal <petrbouchal@users.noreply.github.com>
2016-05-23 10:26:39 +0200

    remove MZP to remove bug

--- scraper.py
+++ scraper.py
@@ -19,7 +19,7 @@ print('Starting scraper...')
 
 activedepts = ['MPO', 'MPSV', 'UV', 'MZd', 'MSMT', 'MF', 'MMR-P', 'MMR-S', 'MV', 'MZe', 'MSp',
                'MO', 'MD', 'MZV', 'CSSZ','FS','UP','NKU','CzechInvest','CS-P','CS-S','CS-S2',
-               'CSI','MZP']
+               'CSI']
 # activedepts = ['MMR-P']
 
 jobsallbodies = []
